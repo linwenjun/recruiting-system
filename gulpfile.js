7f5f26818d225d69b52d12d3b035d0bd537b8244
Wenjun LIN <wenjun.lin@rea-group.com>
2015-11-04 20:11:41 +0800

    Add public dir in gitignore

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './',
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./index.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./public/css/main.css', './index.html'], ['html']);
+});
+
+gulp.task('less', function() {
+  gulp.src('./source/less/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'))
+})
+
+gulp.task('watch-less', function() {
+  gulp.watch(['./source/less/*.less'], ['less'])
+})
+
+gulp.task('default', ['connect', 'watch', 'watch-less'])

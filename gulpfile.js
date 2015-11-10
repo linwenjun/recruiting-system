e29a38c97fb69adf8564484879aae6afdad68f85
Wenjun LIN <linwenjun@163.com>
2015-11-10 13:39:27 +0800

    Refine the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var connect = require('gulp-connect');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var ejs = require("gulp-ejs");
+var gulp = require('./gulp')([
+  'browserify',
+  'connect',
+  'ejs',
+  'html',
+  'less',
+  'watch-ejs',
+  'watch-less',
+  'watch'
+]);
 
 gulp.task('build', ['ejs', 'less', 'browserify']);
 gulp.task('default', ['connect', 'watch', 'watch-less', 'watch-ejs'])

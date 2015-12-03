618dcbff6e48e50e726c8b4842de2e3fe0e47248
ming <zsy498761554@live.com>
2015-12-03 16:30:58 +0800

    Add module logon in page register

--- source/scripts/register.js
+++ source/scripts/register.js
@@ -1,3 +1,18 @@
 var $ = global.jQuery = require('jquery');
 require("bootstrap");
 
+$(function () {
+    $('#change-to-logon').on('click', function () {
+        $('#register').hide();
+        $('#logon').show();
+        $('#register-right').hide();
+        $('#logon-right').show();
+    });
+
+    $('#change-to-register').on('click', function () {
+        $('#logon').hide();
+        $('#register').show();
+        $('#logon-right').hide();
+        $('#register-right').show();
+    });
+});

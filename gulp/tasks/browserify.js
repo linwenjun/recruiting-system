911f67cdf3545246bf318a4cbb4c4e233aac4cb9
Wenjun LIN <linwenjun@163.com>
2015-12-05 22:18:44 +0800

    Add gulp-rename for browserify

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -6,6 +6,7 @@ var es = require('event-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
+var rename = require('gulp-rename');
 
 gulp.task('browserify', function(done) {
     glob('./source/scripts/*.js', function(err, files) {
@@ -18,14 +19,15 @@ gulp.task('browserify', function(done) {
             })
             .transform('babelify', {presets: ['es2015', 'react']})
             .bundle();
-            var file_index = entry.lastIndexOf('/')+1;
-            var new_entry = entry.slice(file_index);
 
-            return a.pipe(source(new_entry))
+            return a.pipe(source(entry))
                     .pipe(buffer())
                     .pipe(sourcemaps.init({loadMaps: true}))
                     .pipe(uglify())
                     .pipe(sourcemaps.write('./'))
+                    .pipe(rename(function(path) {
+                      path.dirname = "";
+                    }))
                     .pipe(gulp.dest('./public/scripts/'));
         });
         es.merge(tasks).on('end', done);

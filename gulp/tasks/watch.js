82e7285d0e719fc66273868845e4794cd6a7b334
ming <zsy498761554@live.com>
2015-12-05 23:15:26 +0800

    Fix big on gulp task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 
 gulp.task('watch', function() {
-  gulp.watch(['./public/css/*.css', './public/scripts/*.js', './index.html'], ['html']);
+  gulp.watch(['./public/css/*.css', './public/scripts/*.js', './*.html'], ['html']);
 });

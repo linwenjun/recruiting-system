201cf4140d897852822fc8a3ae23bfd34d2ecf29
Wenjun LIN <linwenjun@163.com>
2015-12-14 20:17:15 +0800

    Remove out dir

--- api/out/production/api/db/migration/V1__Create_person_table.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-    id int not null primary key AUTO_INCREMENT,
-    email varchar(100) not null,
-    mobilePhone varchar(100) not null,
-    password varchar(100) not null,
-    createDate int(11) not null
-);
---
+++ api/src/main/java/com/thoughtworks/twars/action/UserAction.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.twars.action;
+
+/**
+ * Created by wjlin on 12/14/15.
+ */
+public class UserAction {
+}

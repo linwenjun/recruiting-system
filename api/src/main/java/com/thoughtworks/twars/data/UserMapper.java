be8f228ad9f9a4af1bc48ba4976c714df1896d3e
Wenjun LIN <linwenjun@163.com>
2015-12-16 12:04:36 +0800

    Remove useless api for user

--- api/src/main/java/com/thoughtworks/twars/data/UserMapper.java
+++ api/src/main/java/com/thoughtworks/twars/data/UserMapper.java
@@ -2,8 +2,6 @@ package com.thoughtworks.twars.data;
 
 import com.thoughtworks.twars.bean.User;
 
-import java.util.List;
-
 public interface UserMapper {
 
     public int insertUser(User user);
@@ -12,11 +10,6 @@ public interface UserMapper {
 
     public User getUserByEmail(String email);
 
-    public List<User> getAllUsers();
-
-    public void updateUser(User user);
-
-    public void deleteUser(Integer id);
-
     public User getUserByMobilePhone(String mobilePhone);
+    
 }

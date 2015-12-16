dcddbd6603518bf4ae89de0fff75244228558ad4
wengjiaojiao <419909058@qq.com>
2015-12-16 10:00:06 +0800

    JS:add login router path

--- app/routes/route.js
+++ app/routes/route.js
@@ -2,4 +2,5 @@ exports.setRoutes = function(app) {
   app.use('/', require('./routers/index'));
   app.use('/register', require('./routers/register'));
   app.use('/answer', require('./routers/answer'));
+  app.use('/login', require('./routers/login'));
 };

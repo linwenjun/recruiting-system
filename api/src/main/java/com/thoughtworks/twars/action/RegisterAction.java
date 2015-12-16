9ecc113637180c8f95c69648240a3305a2761b2b
Wenjun LIN <linwenjun@163.com>
2015-12-16 11:36:10 +0800

    Refactory the user register api

--- api/src/main/java/com/thoughtworks/twars/action/RegisterAction.java
+++ api/src/main/java/com/thoughtworks/twars/action/RegisterAction.java
@@ -6,44 +6,31 @@ import com.thoughtworks.twars.data.UserMapper;
 import com.thoughtworks.twars.db.DBUtil;
 import org.apache.ibatis.session.SqlSession;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
-@Path("/user")
-public class UserAction {
-
+@Path("/register")
+public class RegisterAction {
     UserMapper mapper;
     SqlSession session;
 
-    public UserAction() {
+    public RegisterAction() {
         session = DBUtil.getSession();
         mapper = session.getMapper(com.thoughtworks.twars.data.UserMapper.class);
     }
 
-    @GET
-    @Path("/{param}")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUser(@PathParam("param") int userId) {
-
-        User user = mapper.getUserById(userId);
-        return user;
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Map getUserByField(
-            @QueryParam("field") String field,
-            @QueryParam("value") String value
-    ) {
-        User user = null;
-
-        if(field.equals("email")) {
-            user = mapper.getUserByEmail(value);
-        } else if(field.equals("mobilePhone")) {
-            user = mapper.getUserByMobilePhone(value);
-        }
+    public Map createUser(User user) {
+        mapper.insertUser(user);
+        session.commit();
+        session.close();
 
         Map<String, Link> map = new HashMap<>();
         map.put("user", user.getLink());

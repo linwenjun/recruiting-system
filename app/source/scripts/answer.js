aa19fe66610399e99586ee7470bcc4d3ff02ef28
sialvsic <sialvsic@outlook.com>
2015-12-17 15:27:25 +0800

    Fix bug on answer.js

--- app/source/scripts/answer.js
+++ app/source/scripts/answer.js
@@ -1,5 +1,6 @@
 var $ = global.jQuery = require('jquery');
 var ReactDOM = require('react-dom');
+var React = require('react');
 require("bootstrap");
 
 var LogicPuzzle = require('./component/logic-puzzle.component');
